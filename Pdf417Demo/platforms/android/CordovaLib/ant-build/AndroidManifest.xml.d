bbab6d0372d02a25d454f11fb73a7a60c433eb4d
Ljudevit <ljudevit.habjanec@instantcom.net>
2014-09-30 22:38:28 +0200

    Removed CordovaLib from git

--- Pdf417Demo/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/extra/photopay/git/pdf417/PhoneGap/Pdf417Demo/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/extra/photopay/git/pdf417/PhoneGap/Pdf417Demo/platforms/android/CordovaLib/AndroidManifest.xml \
--- Pdf417Demo/platforms/android/CordovaLib/ant-gen/R.java.d
+++
@@ -1,1 +0,0 @@
- : /extra/photopay/git/pdf417/PhoneGap/Pdf417Demo/platforms/android/CordovaLib/ant-build/AndroidManifest.xml \
