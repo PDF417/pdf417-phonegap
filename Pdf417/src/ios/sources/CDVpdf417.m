6de1d593b2978e8c35282a43d472edf27585acc8
Jurica Cerovec <jurica.cerovec@microblink.com>
2015-08-14 18:45:44 +0200

    Added a fix for crash when license key without USDL right is used

--- Pdf417/src/ios/sources/CDVpdf417.m
+++ Pdf417/src/ios/sources/CDVpdf417.m
@@ -31,12 +31,14 @@
 
 @synthesize lastCommand;
 
+- (BOOL)shouldUsePdf417RecognizerForTypes:(NSArray *)types {
+    return [types containsObject:@"PDF417"];
+}
+
 - (PPPdf417RecognizerSettings *)pdf417RecognizerSettingsWithOptions:(NSDictionary *)options
                                                               types:(NSArray *)types {
 
     PPPdf417RecognizerSettings *pdf417RecognizerSettings = [[PPPdf417RecognizerSettings alloc] init];
-
-    [pdf417RecognizerSettings setEnabled:[types containsObject:@"PDF417"]];
 
     // Set this to true to scan barcodes which don't have quiet zone (white area) around it
     // Use only if necessary because it slows down the recognition process
@@ -52,14 +54,24 @@
     return pdf417RecognizerSettings;
 }
 
+- (BOOL)shouldUseUsdlRecognizerForTypes:(NSArray *)types {
+    return [types containsObject:@"USDL"];
+}
+
 - (PPUsdlRecognizerSettings *)usdlRecognizerSettingsWithOptions:(NSDictionary *)options
                                                           types:(NSArray *)types {
 
     PPUsdlRecognizerSettings *usdlRecognizerSettings = [[PPUsdlRecognizerSettings alloc] init];
 
-    [usdlRecognizerSettings setEnabled:[types containsObject:@"USDL"]];
-
     return usdlRecognizerSettings;
+}
+
+- (BOOL)shouldUseBarDecoderRecognizerForTypes:(NSArray *)types {
+
+    BOOL code128 = [types containsObject:@"Code 128"];
+    BOOL code39 = [types containsObject:@"Code 39"];
+
+    return (code128 || code39);
 }
 
 - (PPBarDecoderRecognizerSettings *)barDecoderRecognizerSettingsWithOptions:(NSDictionary *)options
@@ -70,8 +82,6 @@
     BOOL code128 = [types containsObject:@"Code 128"];
     BOOL code39 = [types containsObject:@"Code 39"];
 
-    [barDecoderRecognizerSettings setEnabled:(code128 || code39)];
-
     barDecoderRecognizerSettings.scanCode128 = code128;
     barDecoderRecognizerSettings.scanCode39 = code39;
 
@@ -81,6 +91,20 @@
     barDecoderRecognizerSettings.scanInverse = (scanInverse && [scanInverse boolValue]);
 
     return barDecoderRecognizerSettings;
+}
+
+- (BOOL)shouldUseZxingRecognizerForTypes:(NSArray *)types {
+
+    BOOL aztec = [types containsObject:@"Aztec"];
+    BOOL dataMatrix = [types containsObject:@"Data Matrix"];
+    BOOL ean8 = [types containsObject:@"EAN 8"];
+    BOOL ean13 = [types containsObject:@"EAN 13"];
+    BOOL itf = [types containsObject:@"ITF"];
+    BOOL qrcode = [types containsObject:@"QR Code"];
+    BOOL upca = [types containsObject:@"UPCA"];
+    BOOL upce = [types containsObject:@"UPCE"];
+
+    return (qrcode || ean8 || ean13 || itf || upca || upce || aztec || dataMatrix);
 }
 
 - (PPZXingRecognizerSettings *)zxingRecognizerSettingsWithOptions:(NSDictionary *)options
@@ -96,8 +120,6 @@
     BOOL qrcode = [types containsObject:@"QR Code"];
     BOOL upca = [types containsObject:@"UPCA"];
     BOOL upce = [types containsObject:@"UPCE"];
-
-    [zxingRecognizerSettings setEnabled:(qrcode || ean8 || ean13 || itf || upca || upce || aztec || dataMatrix)];
 
     zxingRecognizerSettings.scanAztec = aztec;
     zxingRecognizerSettings.scanCode128 = NO;
@@ -165,13 +187,21 @@
      */
 
     // Add PDF417 Recognizer setting to a list of used recognizer settings
-    [settings.scanSettings addRecognizerSettings:[self pdf417RecognizerSettingsWithOptions:options types:types]];
+    if ([self shouldUsePdf417RecognizerForTypes:types]) {
+        [settings.scanSettings addRecognizerSettings:[self pdf417RecognizerSettingsWithOptions:options types:types]];
+    }
 
-    [settings.scanSettings addRecognizerSettings:[self zxingRecognizerSettingsWithOptions:options types:types]];
+    if ([self shouldUseZxingRecognizerForTypes:types]) {
+        [settings.scanSettings addRecognizerSettings:[self zxingRecognizerSettingsWithOptions:options types:types]];
+    }
 
-    [settings.scanSettings addRecognizerSettings:[self usdlRecognizerSettingsWithOptions:options types:types]];
+    if ([self shouldUseUsdlRecognizerForTypes:types]) {
+        [settings.scanSettings addRecognizerSettings:[self usdlRecognizerSettingsWithOptions:options types:types]];
+    }
 
-    [settings.scanSettings addRecognizerSettings:[self barDecoderRecognizerSettingsWithOptions:options types:types]];
+    if ([self shouldUseBarDecoderRecognizerForTypes:types]) {
+        [settings.scanSettings addRecognizerSettings:[self barDecoderRecognizerSettingsWithOptions:options types:types]];
+    }
 
     // To specify we want to perform recognition of other barcode formats, initialize the ZXing recognizer settings
     PPZXingRecognizerSettings *zxingRecognizerSettings = [[PPZXingRecognizerSettings alloc] init];
