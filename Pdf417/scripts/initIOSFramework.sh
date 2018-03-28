4b2b323351d43ccf7d53c49c086ab14f260178bb
Jure Cular <jure.cular@microblink.com>
2018-03-28 14:04:38 +0200

    Fixed initIOSFramework script

--- Pdf417/scripts/initIOSFramework.sh
+++ Pdf417/scripts/initIOSFramework.sh
@@ -8,11 +8,11 @@ LINK='https://github.com/PDF417/pdf417-ios/releases/download/v7.0.0/pdf417-ios_v
 FILENAME='pdf417-ios.zip'
 
 # check if Microblink framework and bundle already exist
-wget --version > /dev/null 2>&1 || ( echo "ERROR: couldn't download Microblink framework, install wget" &&  exit 1 )
+wget --version > /dev/null 2>&1 || { echo "ERROR: couldn't download Microblink framework, install wget" &&  exit 1; }
 wget -O "${FILENAME}" "${LINK}" -nv --show-progress || ( echo "ERROR: couldn't download Microblink framework, Something went while downloading framework from ${LINK}" && exit 1 )
 
 echo "Unzipping ${FILENAME}"
-unzip -v > /dev/null 2>&1 || ( echo "ERROR: couldn't unzip Microblink framework, install unzip" && exit 1 )
+unzip -v > /dev/null 2>&1 || { echo "ERROR: couldn't unzip Microblink framework, install unzip" && exit 1; }
 unzip "${FILENAME}" >/dev/null 2>&1 && echo "Unzipped ${FILENAME}"
 
 if [ -d 'Microblink.bundle' ] ; then
